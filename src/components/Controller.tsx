3d902d8da9392eee8fdc419fd24d18ed74cd2c1d
Chanawee Pintaya <Chanawee_P@cmu.ac.th>
2024-08-29 20:44:03 +0700

    Finished

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,26 +1,27 @@
+
 import { LangContext } from "@contexts/LangContext";
 import { ThemeContext } from "@contexts/ThemeContext";
 import { useContext } from "react";
 
 export default function Controller() {
   const { toggleTheme, setDarkTheme, setLightTheme } = useContext(ThemeContext);
-  const { toggleLang } = useContext(LangContext);
+  const { lang,toggleLang } = useContext(LangContext);
   return (
     <div className="d-flex justify-content-center gap-2">
       <button className="btn btn-secondary" onClick={setLightTheme}>
-        {/* Please update this text so it shows in EN or TH */}
+        {lang.buttons.setLightTheme}
         Light
       </button>
       <button className="btn btn-dark" onClick={setDarkTheme}>
-        {/* Please update this text so it shows in EN or TH */}
+        {lang.buttons.setDarkTheme}
         Light Dark
       </button>
       <button className="btn btn-primary" onClick={toggleTheme}>
-        {/* Please update this text so it shows in EN or TH */}
+        {lang.buttons.toggleTheme}
         Light Toggle Theme
       </button>
       <button className="btn btn-danger" onClick={toggleLang}>
-        {/* Please update this text so it shows in EN or TH */}
+        {lang.buttons.toggleLang}
         Light Toggle Language
       </button>
     </div>
